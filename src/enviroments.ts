248576254a0b87beaded8ce91374b65388c58fde
amalGuesmii <amal.guesmi@umanlink.com>
2023-11-22 18:10:09 +0100

    accueil

--- src/enviroments.ts
+++ src/enviroments.ts
@@ -7,17 +7,20 @@ export const environments= {
     usersSignin :"/auth/signin",
     requestResetPwd : '/auth/sendEmail' ,
     resetPwd : '/auth/setPassword' , 
+   
+   
+   
     roles : {
     userRole : "talent" , // used in the signup (the role sent to the backend )
     user : "ROLE_TALENT" , //  (the role receied from the backend )
-
- // used in the add (the role sent to the backend )
+    
+    //  (the role receied from the backend )
     entAdmin : 'ROLE_ENTREPRISE',
     entRecruter : 'ROLE_RECRUTER',
     entEditor : 'ROLE_EDITOR' ,
     entEmployee : 'ROLE_EMPLOYEE' ,
-
- //  (the role receied from the backend )
+    
+    // used in the add (the role sent to the backend )
     entRoleAdmin: 'entreprise',
     entRecruterRole : 'recruter',
     entEditorRole : 'editor' ,
