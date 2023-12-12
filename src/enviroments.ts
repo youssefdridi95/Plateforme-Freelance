2259d555ee9234ecd6bf8c1986bd9b549c5c3cbc
AhmedUmanlink <ahmed.jaidi@umanlink.com>
2023-12-12 19:00:35 +0100

    ..

--- src/enviroments.ts
+++ src/enviroments.ts
@@ -6,8 +6,8 @@ export const environments= {
   //  backendUrl : "http://192.168.195.26:9090" ,
     // webSocketBackendUrl : 'ws://192.168.195.70:9090/ws',
 
-     backendUrl : "http://192.168.195.29:9090" ,
-    //backendUrl : "http://192.168.195.70:9092" ,
+    // backendUrl : "http://192.168.195.29:9090" ,
+    backendUrl : "http://192.168.195.70:9092" ,
       webSocketBackendUrl : 'ws://192.168.195.70:9092/ws',
     connectUser : '/user/connectUser',
     disConnectUser : '/auth/disconnectUser',
